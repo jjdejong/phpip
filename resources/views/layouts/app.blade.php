9bdd1cd2c144c2d0c850b81c50dd7f2fba905bc1
Jean Jacques de Jong <jjdejong@omnipat.fr>
2016-12-21 14:17:40 +0100

    Changed app name to phpIP and localization settings

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'phpIP') }}</title>
 
     <!-- Styles -->
     <link href="/css/app.css" rel="stylesheet">
@@ -36,7 +36,7 @@
 
                     <!-- Branding Image -->
                     <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
+                        {{ config('app.name', 'phpIP') }}
                     </a>
                 </div>
 
