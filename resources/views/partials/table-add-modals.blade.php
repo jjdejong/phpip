810dc1d7d2aa1a85b7dd111fa6402e68438a2b8c
Yves Brungard <yves.brungard_mageia@gadz.org>
2018-05-12 08:06:25 +0200

    Better arrangement in the header of rules edit modal boxes

--- resources/views/partials/table-add-modals.blade.php
+++ resources/views/partials/table-add-modals.blade.php
@@ -3,8 +3,8 @@
 	    <!-- Modal content-->
 	    <div class="modal-content">
 		    <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal">&times;</button>
 				<h4 class="modal-title">Title placeholder</h4>
+				<button type="button" class="close" data-dismiss="modal">&times;</button>
 			</div>
 			<div class="modal-body">
 				Ajax placeholder
