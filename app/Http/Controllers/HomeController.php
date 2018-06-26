0bf9d97b331de6f913e2a9f442beeccaf5eb7333
Jean Jacques de Jong <jjdejong@omnipat.fr>
2018-06-26 16:25:25 +0200

    Fixed a few issues with the task lists in the dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,13 +34,13 @@ class HomeController extends Controller
         ->where('done','0')->where('code', '!=', 'REN');
         if (! is_null($MyTasks)) {
             if ($MyTasks =='1') $tasks = $tasks->where('assigned_to','=',Auth::user()->login);}
-        $tasks = $tasks->orderby('due_date')->get();
+        $tasks = $tasks->orderby('due_date')->simplePaginate(25);
         // Get list of active renewals
         $renewals = Task::with('info','trigger', 'trigger.matter')
         ->where('done','0')->where('code', '=', 'REN');
         if (! is_null($MyRenewals)) {
             if ($MyRenewals =='1') $renewals = $renewals->where('assigned_to','=',Auth::user()->login);}
-        $renewals = $renewals->orderby('due_date')->get();
+        $renewals = $renewals->orderby('due_date')->simplePaginate(25);
         
         return view('home',compact('tasks','renewals'));
     }
