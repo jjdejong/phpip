aa6be1f879a5b39ca968e61f50df3ca9fee250c6
Jean Jacques de Jong <jjdejong@omnipat.fr>
2019-04-04 13:40:52 +0200

    Further disabling of Vanturecraft/Revisionable features

--- app/Actor.php
+++ app/Actor.php
@@ -12,11 +12,11 @@ class Actor extends Model
   protected $hidden = ['login', 'last_login', 'password', 'remember_token', 'creator', 'updated', 'updater'];
   protected $guarded = ['id', 'password', 'creator', 'updated', 'updater'];
 
-  //use \Venturecraft\Revisionable\RevisionableTrait;
-  protected $revisionEnabled = true;
-  protected $revisionCreationsEnabled = true;
-  protected $revisionCleanup = true; //Remove old revisions (works only when used with $historyLimit)
-  protected $historyLimit = 500; //Maintain a maximum of 500 changes at any point of time, while cleaning up old revisions.
+  // use \Venturecraft\Revisionable\RevisionableTrait;
+  // protected $revisionEnabled = true;
+  // protected $revisionCreationsEnabled = true;
+  // protected $revisionCleanup = true; //Remove old revisions (works only when used with $historyLimit)
+  // protected $historyLimit = 500; //Maintain a maximum of 500 changes at any point of time, while cleaning up old revisions.
 
   public function company() {
   	return $this->belongsTo('App\Actor', 'company_id');
--- app/Event.php
+++ app/Event.php
@@ -11,12 +11,11 @@ class Event extends Model
     protected $hidden = ['creator', 'updated', 'updater'];
     protected $guarded = ['id', 'creator', 'updated', 'updater'];
 
-    //use \Venturecraft\Revisionable\RevisionableTrait;
-
-    protected $revisionEnabled = true;
-    protected $revisionCreationsEnabled = true;
-    protected $revisionCleanup = true; //Remove old revisions (works only when used with $historyLimit)
-    protected $historyLimit = 500; //Maintain a maximum of 500 changes at any point of time, while cleaning up old revisions.
+    // use \Venturecraft\Revisionable\RevisionableTrait;
+    // protected $revisionEnabled = true;
+    // protected $revisionCreationsEnabled = true;
+    // protected $revisionCleanup = true; //Remove old revisions (works only when used with $historyLimit)
+    // protected $historyLimit = 500; //Maintain a maximum of 500 changes at any point of time, while cleaning up old revisions.
 
     public function info() {
         return $this->belongsTo('App\EventName', 'code');
