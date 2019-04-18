61dbb60348aa0bd957234e32ee5040878e5163fc
Jean Jacques de Jong <jjdejong@omnipat.fr>
2019-04-18 15:18:49 +0200

    Fixed file name

--- database/seeds/MatterSampleSeeder.php
+++ database/seeds/MatterSampleSeeder.php
@@ -14,7 +14,7 @@ class MatterSampleSeeder extends Seeder
     {
         require 'matter-sample.php';
         App\Matter::insert($matter);
-        require 'matter_actor_lnk.php';
+        require 'matter_actor_lnk-sample.php';
         DB::table('matter_actor_lnk')->insert($matter_actor_lnk);
     }
 }
