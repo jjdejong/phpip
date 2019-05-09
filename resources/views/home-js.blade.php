b821c4e49d56433b15f5de63b2fd37c37dd65fed
Jean Jacques de Jong <jjdejong@omnipat.fr>
2019-05-09 08:55:17 +0200

    Code reformatting

--- resources/views/home-js.blade.php
+++ resources/views/home-js.blade.php
@@ -2,96 +2,100 @@
     .card-body {
         max-height: 250px;
         overflow: auto;
-        }
+    }
     .card-body .row:hover {
         background-color: #eeeeee;
-        }
+    }
 </style>
 <script>
 
-function refreshTasks() {
-    var url = '/home?' + $("#filter").find("input").filter(function(){return $(this).val().length > 0}).serialize(); // Filter out empty values
-    $('#tasklist').load(url + ' #tasklist > div', function() { // Refresh all the card-body's in #tasklist
-	window.history.pushState('', 'phpIP' , url);
-	});
-}
+    function refreshTasks() {
+        var url = '/home?' + $("#filter").find("input").filter(function () {
+            return $(this).val().length > 0;
+        }).serialize(); // Filter out empty values
+        $('#tasklist').load(url + ' #tasklist > div', function () { // Refresh all the card-body's in #tasklist
+            window.history.pushState('', 'phpIP', url);
+        });
+    }
 
-function refreshRenewals() {
-    var url = '/home?' + $("#filter").find("input").filter(function(){return $(this).val().length > 0}).serialize(); // Filter out empty values
-    $('#renewallist').load(url + ' #renewallist > div', function() { // Refresh all the card-body in #renewallist
-	window.history.pushState('', 'phpIP' , url);
-	});
-}
+    function refreshRenewals() {
+        var url = '/home?' + $("#filter").find("input").filter(function () {
+            return $(this).val().length > 0;
+        }).serialize(); // Filter out empty values
+        $('#renewallist').load(url + ' #renewallist > div', function () { // Refresh all the card-body in #renewallist
+            window.history.pushState('', 'phpIP', url);
+        });
+    }
 
-$('#mytasks').on('change', function() {
-  refreshTasks();
-});
+    $('#mytasks').on('change', function () {
+        refreshTasks();
+    });
 
-$('#alltasks').on('change', function() {
-  refreshTasks();
-});
+    $('#alltasks').on('change', function () {
+        refreshTasks();
+    });
 
-$('#allrenewals').on('change', function() {
-  refreshRenewals();
-});
+    $('#allrenewals').on('change', function () {
+        refreshRenewals();
+    });
 
-$('#myrenewals').on('change', function() {
-  refreshRenewals();
-});
+    $('#myrenewals').on('change', function () {
+        refreshRenewals();
+    });
 
-	// Ajax fill the opened modal and set global parameters
-    $("#homeModal").on("show.bs.modal", function(event) {
-    	relatedUrl = $(event.relatedTarget).attr("href");
-    	size = $(event.relatedTarget).data("size");
+    // Ajax fill the opened modal and set global parameters
+    $("#homeModal").on("show.bs.modal", function (event) {
+        relatedUrl = $(event.relatedTarget).attr("href");
+        size = $(event.relatedTarget).data("size");
 
-    	$(this).find(".modal-title").text( $(event.relatedTarget).attr("title") );
-    	$(this).find(".modal-dialog").removeClass("modal-lg");
-    	$(this).find(".modal-dialog").addClass(size);
+        $(this).find(".modal-title").text($(event.relatedTarget).attr("title"));
+        $(this).find(".modal-dialog").removeClass("modal-lg");
+        $(this).find(".modal-dialog").addClass(size);
         $(this).find(".modal-body").load(relatedUrl);
     });
 
-    $('#clear-ren-tasks').click(function(){
+    $('#clear-ren-tasks').click(function () {
         var tids = new Array();
-        $('.clear-ren-task').each(function(){
-          if($(this).is(':checked'))
-             tids.push($(this).attr('id'));
+        $('.clear-ren-task').each(function () {
+            if ($(this).is(':checked'))
+                tids.push($(this).attr('id'));
         });
-        if(tids.length === 0) {
-             alert("No tasks selected for clearing!");
-             return;
-        }
-		$.post('/matter/clear-tasks', 
-			{ task_ids: tids, done_date: $('#renewalcleardate').val() },
-			function(response){
-                if(response.errors === '') {
-                refreshRenewals();}
-                else {
-                    alert(response.errors.done_date);
-                }
-			}
-		);
-    });
-
-    $('#clear-open-tasks').click(function(){
-        var tids = new Array();
-        $('.clear-open-task').each(function(){
-          if($(this).is(':checked'))
-             tids.push($(this).attr('id'));
-        });
-        if(tids.length === 0) {
-             alert("No tasks selected for clearing!");
-             return;
+        if (tids.length === 0) {
+            alert("No tasks selected for clearing!");
+            return;
         }
         $.post('/matter/clear-tasks',
-			{ task_ids: tids, done_date: $('#taskcleardate').val() },
-			function(response){
-                if(response.errors === '') {
-                refreshTasks();}
-                else {
-                    alert(response.errors.done_date);
+                {task_ids: tids, done_date: $('#renewalcleardate').val()},
+                function (response) {
+                    if (response.errors === '') {
+                        refreshRenewals();
+                    } else {
+                        alert(response.errors.done_date);
+                    }
                 }
-			}
-		);
+        );
+    });
+
+    $('#clear-open-tasks').click(function () {
+        var tids = new Array();
+        $('.clear-open-task').each(function () {
+            if ($(this).is(':checked'))
+                tids.push($(this).attr('id'));
+        });
+        if (tids.length === 0) {
+            alert("No tasks selected for clearing!");
+            return;
+        }
+        $.post('/matter/clear-tasks',
+                {task_ids: tids, done_date: $('#taskcleardate').val()},
+                function (response) {
+                    if (response.errors === '') {
+                        refreshTasks();
+                    } else {
+                        alert(response.errors.done_date);
+                    }
+                }
+        );
     });
 
 </script>
