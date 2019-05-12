7195cce8f8e688ca2a18e473e81a5b5e91c20050
Papoteur <papoteur@mageia.org>
2019-05-12 19:50:55 +0200

    Search field only when authenticated

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,7 +28,7 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'phpIP') }}
                 </a>
-            
+                @auth
                 <form method="POST" action="/matter/search">
                     @csrf
                     <div class="input-group">
@@ -42,6 +42,7 @@
                         </div>
                     </div>
                 </form>
+                @endauth
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
