fb1a6d8da72202075b7a855103562baa95f6b97e
Jean Jacques de Jong <jjdejong@omnipat.fr>
2019-11-05 09:56:22 +0100

    Provided an unattended installation script for Ubuntu Bionic

---
+++ doc/install-phpip-bionic.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# Uncomment if user should not be prompted for a MySQL password
+# export DEBIAN_FRONTEND=noninteractive
+echo "
+********************************
+Updating Ubuntu
+********************************"
+sudo add-apt-repository universe
+apt update
+apt -y upgrade
+echo "
+********************************
+Installing Apache, MySQL, PHP 
+********************************"
+apt -y install lamp-server^ php7.2-simplexml php7.2-mbstring unzip git-core composer
+# sed -i "s/^#application\/x-httpd-php/application\/x-httpd-php/" /etc/mime.types
+echo "CREATE DATABASE phpip; GRANT ALL PRIVILEGES ON phpip.* TO phpip@localhost IDENTIFIED BY 'phpip';" | mysql
+a2enmod rewrite
+echo "127.0.0.1    phpip.local" >> /etc/hosts
+echo "
+********************************
+Getting phpIP from GitHub
+********************************"
+cd /var/www/html
+git clone https://github.com/jjdejong/phpip-v2.git phpip
+cd phpip
+php artisan key:generate
+php artisan config:clear
+cp doc/phpip.conf /etc/apache2/sites-enabled/
+service apache2 reload
+cp .env.example .env
+echo "
+********************************
+Installing database
+********************************"
+php artisan migrate --seed
+echo "Install finished. If you want to populate the database with sample data run
+php artisan db:seed --class=SampleSeeder
+Go to http://phpip.local and login with the credentials phpipuser:changeme"
