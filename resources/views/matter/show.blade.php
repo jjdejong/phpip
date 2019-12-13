618e98f9360410e83a473fff26cb90d0f8a26f26
Jean Jacques de Jong <jjdejong@omnipat.fr>
2019-12-13 09:30:29 +0100

    Extended display of warning of special actors to all roles in matter.show

--- resources/views/matter/show.blade.php
+++ resources/views/matter/show.blade.php
@@ -129,8 +129,8 @@ $linkedBy = $matter->linkedBy->groupBy('type_code');
           <ul class="list-unstyled mb-0">
             @foreach ( $role_group as $actor)
             <li class="text-truncate {{ $actor->inherited ? 'font-italic' : '' }}">
-              @if ( $actor->warn && $role_name == 'Client' )
-              <span title="Payment Difficulties">&#9888;</span>
+              @if ( $actor->warn )
+              <span title="Special instructions">&#9888;</span>
               @endif
               <a href="/actor/{{ $actor->actor_id }}"
                 data-toggle="modal"
