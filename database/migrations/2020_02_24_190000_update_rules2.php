2326b2e68ab75fe7771e5cdf786639a759e51057
Jean Jacques de Jong <jjdejong@omnipat.fr>
2020-02-24 19:01:09 +0100

    Some more task rules seeding glitches. "Draft by" task should not be limited to French patents!

--- database/migrations/2020_02_24_190000_update_rules2.php
+++ database/migrations/2020_02_24_190000_update_rules2.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateRules extends Migration
+class UpdateRules2 extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,8 @@ class UpdateRules extends Migration
     public function up()
     {
         DB::table('task_rules')->where('id', 3)->update(['detail' => 'Clear']);
-        DB::table('task_rules')->where('id', 5)->update(['for_origin' => NULL, 'detail' => 'Clear']);
+        DB::table('task_rules')->where('id', 5)->update(['for_origin' => NULL, 'for_country' => NULL, 'detail' => 'Clear']);
+        DB::table('task_rules')->where('id', 24)->update(['for_country' => NULL]);
         DB::table('task_rules')->where('id', 25)->update(['detail' => 'Delete']);
         DB::table('task_rules')->where('id', 29)->update(['detail' => 'Clear']);
         DB::table('task_rules')->where('id', 38)->update(['detail' => 'Clear']);
