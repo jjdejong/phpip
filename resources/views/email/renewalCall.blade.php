5bf6d97f06a62ef45886ea536762ea06340fbf9b
Jean Jacques de Jong <jjdejong@omnipat.fr>
2020-06-17 16:52:07 +0200

    Further tweaking the renewal call email

--- resources/views/email/renewalCall.blade.php
+++ resources/views/email/renewalCall.blade.php
@@ -4,21 +4,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 </head>
-<body style="font-family: Avenir, Helvetica, sans-serif; box-sizing: border-box; background-color: #f5f8fa; color: #74787E; height: 100%; hyphens: auto; line-height: 1.4; margin: 0; -moz-hyphens: auto; -ms-word-break: break-all; width: 100% !important; -webkit-hyphens: auto; -webkit-text-size-adjust: none; word-break: break-word;">
+<body>
     <style>
-        @media only screen and (max-width: 600px) {
-            .inner-body {
-                width: 100% !important;
-            }
-            .footer {
-                width: 100% !important;
-            }
-        }
-        @media only screen and (max-width: 500px) {
-            .button {
-                width: 100% !important;
-            }
-        }
         table, th, td {
             border: 1px solid black;
             border-collapse: collapse;
