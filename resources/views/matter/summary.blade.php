a3ee1b7a22faa5622d1d53139206b4295a9af2b4
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-05 10:33:48 +0800

    Delete resources directory

--- resources/views/matter/summary.blade.php
+++
@@ -1,8 +0,0 @@
-<div id="tocopy">
-@foreach ($description as $line) 
-{{ $line }} <BR />
-@endforeach
-</div>
-<div class="float-right">
-  <button id="sumButton"  type="button" class="btn btn-primary float-center" data-dismiss="modal">Copy</button>
-</div>
