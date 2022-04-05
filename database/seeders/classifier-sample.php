221dd63bc2977228c5deb80a214d2801cfb0f642
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-05 10:32:02 +0800

    Delete database directory

--- database/seeders/classifier-sample.php
+++
@@ -1,5 +0,0 @@
-<?php
-$classifier = array(
-  array('id' => 4, 'matter_id' => 4, 'type_code' => 'TITOF', 'value' => 'Trip planning with energy constraint'),
-  array('id' => 5, 'matter_id' => 4, 'type_code' => 'TIT', 'value' => 'Trip Planning with Energy Constraint')
-);
