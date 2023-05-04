5d537ddc8b7b8245ddadd821767489a5817a7708
Jean Jacques de Jong <jjdejong@omnipat.fr>
2023-05-04 16:57:08 +0200

    Extracted JS from actor index into separate file

--- resources/views/actor/index.blade.php
+++ resources/views/actor/index.blade.php
@@ -71,32 +71,5 @@
 @endsection
 
 @section('script')
-<script>
-
-  var url = new URL(window.location.href);
-
-  function refreshActorList() {
-    window.history.pushState('', 'phpIP', url)
-    reloadPart(url, 'actorList');
-  }
-
-  person.onchange = (e) => {
-    if (e.target.value.length === 0) {
-      url.searchParams.delete(e.target.name);
-    } else {
-      url.searchParams.set(e.target.name, e.target.value);
-    }
-    refreshActorList();
-  }
-
-  filterFields.addEventListener('input', debounce( e => {
-    if (e.target.value.length === 0) {
-      url.searchParams.delete(e.target.name);
-    } else {
-      url.searchParams.set(e.target.name, e.target.value);
-    }
-    refreshActorList();
-  }, 300));
-
-</script>
+<script src="{{ asset('js/actor-index.js') }}"></script>
 @endsection
