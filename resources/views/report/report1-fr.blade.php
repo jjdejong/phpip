3df06b6f80afc239238c9692b22f06ca9ba71899
Papoteur <yves.brungard_git@gadz.org>
2023-05-12 14:46:04 +0200

    improve report in French

--- resources/views/report/report1-fr.blade.php
+++ resources/views/report/report1-fr.blade.php
@@ -27,83 +27,115 @@
             font-size: 80%;
             font-style: italic;
         }
+        @media print {
+            .pagebreak { 
+                break-inside: avoid;
+            }
+        }
     </style>
-@php
+    
+    @php
     $previous_cat = "";
     $previous_family = "";
-@endphp
-@foreach ($matters as $matter)
-    @if ($matter['Cat'] !== $previous_cat)
-        @if ($previous_cat != "")
-        </tbody>
-    </table>
-        <hr />
-        @endif
-        @switch( $matter['Cat'])
-            @case('PAT')     
-                <h1>Brevets</h1><br>
-                @break
-            @case('TM')     
-                <h1>Marques</h1><br>
-                @break
-            @case('DM')     
-                <h1>Dessins & modèles</h1><br>
-        @endswitch
-        @php
-            $previous_cat = $matter['Cat'];
-            $new_cat = true;
-        @endphp
-    @endif
-    @if (! str_starts_with($matter['Ref'], $previous_family) || $previous_family == "" || $new_cat)
-        @if ($previous_family != "" && ! $new_cat)
-        </tbody>
-    </table>
-        @endif
-        <hr />
-        @if (  $matter['Title']  !== null)
-        <h2>{!! $matter['Title'] !!}</h2><br>
-        @endif
-        @if ( $matter['Title2'] !== null)
-        <h2>{!! $matter['Title2'] !!}</h2><br>
-        @endif
-        @if ( $matter['Title3'] !== null)
-        <h2>{!! $matter['Title3'] !!}</h2><br>
-        @endif
-        Titulaire: {!! $matter['Applicant'] !!}<br>
-        @if ( $matter['Inventor1'] !== "")
-        Inventeur : {!! $matter['Inventor1'] !!}<br>
-        @endif        
-    <table class="inner-body" cellpadding="0" cellspacing="0">
-        <!-- Body content -->
-        <thead>
-            <tr>
-                <th>Votre réf.</th><th>Code pays</th><th>Dépôt</th><th>Publication</th><th>Délivrance</th><th>Statut</th><th>Notre réf.</th>
-            </tr>
-        </thead>
-        <tbody>
-    @endif
-    @php
-        $previous_family = substr($matter['Ref'], 0, strpos($matter['Ref'], $matter['country']));
-        $new_cat = false;
     @endphp
-            <tr>
-                <td>{!! $matter['ClRef'] !!}</td>
-                <td>{!! $matter['country'] !!}</td>
-                <td>{!! $matter['FilNo'] !!}<br /><span class="tdate">{!! Carbon\Carbon::parse($matter['Filed'])->isoFormat('LL')  !!}</span></td>
-                <td>{!! $matter['PubNo'] !!}<br /><span class="tdate">{!! Carbon\Carbon::parse($matter['Published'])->isoFormat('LL') !!}</span></td>
-                <td>{!! $matter['GrtNo'] !!}<br /><span class="tdate">{!! Carbon\Carbon::parse($matter['Granted'])->isoFormat('LL') !!}</span></td>
-                <td>@if (  $matter['dead'] == 1 )
-                    Fermé : {!! $matter['Status'] !!}
-                @else
-                    {!! $matter['Status'] !!}
-                @endif
+    @foreach ($matters as $matter)
+        @if (  $matter['dead'] == 0 )
+        @if ($matter['Cat'] !== $previous_cat)
+            @if ($previous_cat != "")
+                </tbody>
+                </table>
+                </div>
+                <hr />
+            @endif
+            
+            @switch( $matter['Cat'])
+                @case('PAT')     
+                    <h1>Brevets</h1><br>
+                        @break
+                @case('TM')     
+                    <h1>Marques</h1><br>
+                        @break
+                @case('DM')     
+                    <h1>Dessins & modèles</h1><br>
+            @endswitch
+            
+            @php
+                $previous_cat = $matter['Cat'];
+                $new_cat = true;
+            @endphp
+        @endif
+        
+        @if (! str_starts_with($matter['Ref'], $previous_family) || $previous_family == "" || $new_cat)
+            @if ($previous_family != "" && ! $new_cat)
+                </tbody>
+                </table>
+                </div>
+               <!-- <hr />-->
+            @endif
+            <hr />
+            
+            <div class="pagebreak">
+            
+            @if (  $matter['Title']  !== null)
+                <h2>{!! $matter['Title'] !!}</h2><br>
+            @endif
+            @if ( $matter['Title2'] !== null)
+                <h2>{!! $matter['Title2'] !!}</h2><br>
+            @endif
+            @if ( $matter['Title3'] !== null)
+                <h2>{!! $matter['Title3'] !!}</h2><br>
+            @endif
+            
+            Titulaire: {!! $matter['Applicant'] !!}<br>
+            
+            @if ( $matter['Inventor1'] !== "")
+                Inventeur : {!! $matter['Inventor1'] !!}<br>
+            @endif     
+            
+            <table class="inner-body" cellpadding="0" cellspacing="0">
+                <!-- Body content -->
+                <thead>
+                    <tr>
+                        <th>Votre réf.</th>
+                        <th>Code pays</th>
+                        <th>Dépôt</th>
+                        <th>Publication</th>
+                        <th>Délivrance</th>
+                        <th>Statut</th>
+                        <th>Prochain renouvellement</th>
+                        <th>Notre réf.</th>
+                    </tr>
+                </thead>
+                <tbody>
+        @endif
+        
+        @php
+            $previous_family = substr($matter['Ref'], 0, strpos($matter['Ref'], $matter['country']));
+            $new_cat = false;
+        @endphp
+        <tr>
+            <td>{!! $matter['ClRef'] !!}</td>
+            <td>{!! $matter['country'] !!}</td>
+            <td>{!! $matter['FilNo'] !!}<br /><span class="tdate">{!! empty($matter['Filed'])? "" : Carbon\Carbon::parse($matter['Filed'])->isoFormat('LL')  !!}</span></td>
+                <td>{!! $matter['PubNo'] !!}<br /><span class="tdate">{!! empty($matter['Published'])? "" : Carbon\Carbon::parse($matter['Published'])->isoFormat('LL') !!}</span></td>
+                <td>{!! $matter['GrtNo'] !!}<br /><span class="tdate">{!! empty($matter['Granted'])? "" : Carbon\Carbon::parse($matter['Granted'])->isoFormat('LL') !!}</span></td>
+                <td>
+                    @if (  $matter['dead'] == 1 )
+                        Fermé : {!! $matter['Status'] !!}
+                    @else
+                        {!! $matter['Status'] !!}
+                    @endif
                 </td>
-                <td>{!! $matter['Ref'] !!}</td>
-            </tr>
-@endforeach
-        </tbody>
+                <td><span class="tdate">{!! empty($matter['next'])? "" : Carbon\Carbon::parse($matter['next'])->isoFormat('LL') !!}</span></td>
+            <td>{!! $matter['Ref'] !!}</td>
+        </tr>
+        @endif
+    @endforeach
+    </tbody>
     </table>
+    </div>
     <hr />
 </body>
 </html>
- 
+                    
+                    
