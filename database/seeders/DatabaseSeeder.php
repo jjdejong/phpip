283b437b233685803ace8493750dfb35008d7b07
Jean Jacques de Jong <jjdejong@omnipat.fr>
2025-04-21 00:45:32 +0200

    Fix #159

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,14 +9,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call(CountryTableSeeder::class);
-        $this->call(MatterCategoryTableSeeder::class);
-        $this->call(ClassifierTypeTableSeeder::class);
-        $this->call(ActorRoleTableSeeder::class);
         $this->call(ActorTableSeeder::class);
-        $this->call(EventNameTableSeeder::class);
-        $this->call(MatterTypeTableSeeder::class);
         $this->call(FeesTableSeeder::class);
-        $this->call(TaskRulesTableSeeder::class);
         $this->call(TemplateClassesTableSeeder::class);
         $this->call(TemplateMembersTableSeeder::class);
         $this->call(TranslatedAttributesSeeder::class);
