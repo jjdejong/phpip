f62121655106695923ca57cf097fd9fecf038421
Jean J. de Jong <jj@de-jong.fr>
2025-06-02 09:39:58 +0200

    Infrastructure backend and frontend update.

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,9 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel([
-            //'resources/css/app.css',
             'resources/js/app.js',
         ]),
-    ],
+    ]
 });
 
 
